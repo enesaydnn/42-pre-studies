226be7730019068a95ca81879ee3a196da8eddb8
Enes Aydın <148777354+enesaydnn@users.noreply.github.com>
2025-03-19 23:10:11 +0300

    Update aff_last_param.c

--- exam00/ex10/aff_last_param.c
+++ exam00/ex10/aff_last_param.c
@@ -19,8 +19,7 @@ $> ./aff_last_param
 $
 */
 
-#include <unistd.h>
-void ft_putchar (char *str)
+void ft_putchar(char *str)
 {
     int i = 0;
     while (str[i] != '\0')
@@ -28,18 +27,19 @@ void ft_putchar (char *str)
         write(1,&str[i],1);
         i++;
     }
-
 }
 
-int main(int ac, char **av)
+
+
+int main (int ac, char **av)
 {
-   int i = ac -1;
-   if (ac > 1)
-   {
+    int i = ac -1;
+    while (ac > 1)
+    {
         ft_putchar(av[i]);
         write(1,"\n",1);
-        i--;
-   }
-   write(1,"\n",1);
-   return (0);
+        i++;
+    }
+    write(1,"\n",1);
+    return (0);
 }
