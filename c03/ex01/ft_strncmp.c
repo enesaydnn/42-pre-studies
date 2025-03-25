f383f19e947faa5048be6c68f475a09eaefd496c
Enes Aydın <148777354+enesaydnn@users.noreply.github.com>
2025-03-25 13:25:47 +0300

    Update ft_strncmp.c

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 int     ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i = 0;
-    while (i < n && s1[i] == s2[i])
+    int i = 0;
+    while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
     {
         i++;
     }
-    if(i == n)
+
+    if (i == n)
     {
         return (0);
     }
-    return s1[i] - s2[i];   
+
+    return s1[i] - s2[i];
 }
 
-int main ()
+
+int main()
 {
-    printf("Ascii Değer Farkı : %d \n",ft_strncmp("code","coder",5));
+    printf("Ascii Değer Farkı : %d\n",ft_strncmp("code","coder",4));
     return (0);
 }
