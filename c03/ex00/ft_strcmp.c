9840ac00448abae64a70e032f3563db6fb278016
Enes Aydın <148777354+enesaydnn@users.noreply.github.com>
2025-03-25 12:53:30 +0300

    Update ft_strcmp.c

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-int     ft_strcmp(char *s1, char *s2)
+int ft_strcmp(char *s1, char *s2)
 {
     int i = 0;
-    while (s1[i] == s2[i])
+    while (s1[i] && s2[i] && s1[i] == s2[i])
     {
         i++;
     }
-    return s1[i] - s2[i];
+    return (s1[i] - s2[i]);
 }
 
 int main ()
 {
-    printf("Ascii Değer Farkı : %d \n",ft_strcmp("code", "coder"));
+    printf("Ascii Değer Farkı : %d \n",ft_strcmp("abc", "abc"));
     return (0);
 }
